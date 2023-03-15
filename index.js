ef6ba87711127881af4eadd4405a1254de6b99ae
DESKTOP-MBRQ737\Eva <evastylova@gmail.com>
2023-03-15 20:17:29 +0100

    Minutka DU index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log('funguju!');
 const alarmRing = () => {
     let alarmElm = document.querySelector('.alarm');
     alarmElm.classList.add('alarm--ring');
-    console.log('5 vteřin uběhlo, zvoní');
+    console.log('prodleva uběhla, zvoní');
 }
 
 //setTimeout(alarmRing, 5000);
